0ce11b75f8583db560d7262e2d6389244a27a101
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-28 01:41:46 -0300

    mudando o armazenamento de array para session

--- backend/Usuario.php
+++ backend/Usuario.php
@@ -6,50 +6,43 @@ class Usuario
     public $email; /* email do usuario */
     public $senha; /* senha do usuario */
 
-    public static $lista = []; /* array para a inclusao dos dados e a verificação */
-
-    public function __construct($nome, $email, $senha) /* recebimento dos dados dos novos usuarios */
+    public function __construct() /* recebimento dos dados dos novos usuarios */
     {
-        $this->nome = $nome; /* incluindo os valores dentro das variaveis */
-        $this->email = $email;
-        $this->senha = $senha;
+        $this->nome = ""; /* incluindo os valores dentro das variaveis */
+        $this->email = "";
+        $this->senha = "";
+
+        if (!isset($_SESSION['usuarios'])) {
+            $_SESSION['usuarios'] = [];
+        }
     }
 
-    public function verificar() /* funcao de verificação */
+    public function enviarDados($nome, $email, $senha) /* funcao para o envio de dados */
     {
-        foreach (self::$lista as $conta) { /* chamando o array e gerando uma variavel que vai percorrer por dentro dela  */
-            if ($conta->email == $this->email && $conta->senha == $this->senha) {  /* fazendo a vericação das variaveis que estao dentro do array com os dados do usuario */
-                return true; /* retorno true para podermos pegar o resultado da verificaçao em outra funcao */
+        if (!isset($_SESSION['usuarios'])) {
+            $_SESSION['usuarios'] = [];
+        }
+
+        $_SESSION['usuarios'][] = [
+            'nome' => $nome,
+            'email' => $email,
+            'senha' => $senha
+        ];
+
+        return true;
+    }
+
+    public function verificar() /* funcao de verificação */ {
+        if(!isset($_SESSION['usuarios'])){
+            return false;
+        }
+
+        foreach($_SESSION['usuarios'] as $user){
+            if($user['email'] == $this->email && $user['senha'] == $this->senha){
             }
+            return true;
         }
-        return false; /* retorno false no final da funcao para nao fechar sempre com o false o loop */
-    }
 
-    public function enviarDados() /* funcao para o envio de dados */
-    {
-
-        self::$lista[] = $this; /* nesse caso, o '$this' é o envio dos dados que foram inseridos na criação do novo usuario, la no construct */
-
-        echo "Dados enviados com sucesso...<br>"; /* confirmação na tela de envio */
-    }
-
-    public function acesso() /* funcao confirmando se pode ou nao entrar no app */
-    {
-        if ($this->verificar()) { /* é aqui que pegamos o resultado em boolean da funcao verificar */
-            echo "Acesso liberado<br>"; /* mensagem de liberação */
-        } else {
-            echo "Acesso negado<br>";
-        }
+        return false;
     }
 }
-
-$a = new Usuario("Adriel", "adriel123@gmail.com", 123123); /* exemplo de inclusao dos dados no novo usuario... */
-$a->verificar(); /* aqui nao precisamos mais enviar dados, pq o '$this' ja envia automaticamente para o verificar */
-$a->acesso();
-$a->enviarDados();
-$a->verificar();
-$a->acesso();
-
-$b = new Usuario("Adriel", "adriel123@gmail.com", 123123);
-$b->verificar();
-$b->acesso();
